78cd6c9b62b4f69497fe655ec811e8195db11db9
Justwouter <wouter2312@gmail.com>
2023-09-27 13:50:38 +0200

    Moving/rotating bean

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-        
+using Unity.Mathematics;
+
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour {
+    Vector3 _movement = Vector3.zero;
+    [SerializeField] float _speed = 10;
+    private float _rotationDirection = 0;
+    [SerializeField] float _rotationSpeed = 10;
+
+
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    } 
+    void Update() {
+        transform.Translate(_speed * Time.deltaTime * _movement);
+        transform.Rotate(_rotationDirection*Vector3.up);
+    }
+
+    void OnMove(InputValue inputValue) {
+        _movement = inputValue.Get<Vector3>();
+    }
+
+    void OnRotate(InputValue inputValue){
+        _rotationDirection = inputValue.Get<Vector2>().x;
+        Debug.Log(_rotationDirection);
+
+    }
 }
