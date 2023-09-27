fd29593d3ec7e96ec882647b6659edf96c168742
Justwouter <wouter2312@gmail.com>
2023-09-27 15:25:01 +0200

    Save

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,36 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
-
-using Unity.Mathematics;
+using System.Runtime.CompilerServices;
 
 using UnityEngine;
-using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour {
     Vector3 _movement = Vector3.zero;
-    [SerializeField] float _speed = 10;
-    private float _rotationDirection = 0;
-    [SerializeField] float _rotationSpeed = 10;
+    [SerializeField] float _speed = 10f;
+    [SerializeField] float _rotationSpeed = 100f;
+    [SerializeField] float _jumpHight = 10f;
+    private float _rotationDirection = 0f;
+    private Rigidbody _rb;
+
 
 
     void Start() {
-
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update() {
         transform.Translate(_speed * Time.deltaTime * _movement);
-        transform.Rotate(_rotationDirection*Vector3.up);
+        transform.Rotate(_rotationDirection * _rotationSpeed * Time.deltaTime * Vector3.up);
+        CheckInAir();
     }
 
     void OnMove(InputValue inputValue) {
         _movement = inputValue.Get<Vector3>();
     }
 
-    void OnRotate(InputValue inputValue){
+    void OnRotate(InputValue inputValue) {
         _rotationDirection = inputValue.Get<Vector2>().x;
-        Debug.Log(_rotationDirection);
 
+    }
+
+
+    // State 2, Jumping
+    private bool _isJumping = false;
+    void OnJump(InputValue inputValue) {
+        if (!_isJumping) {
+            _isJumping = true;
+            _rb.AddForce(new Vector3(0, _jumpHight, 0), ForceMode.Impulse);
+        }
+    }
+
+    void CheckInAir() {
+        Debug.DrawRay(transform.position, new Vector3(0,100,0), Color.red);
+    }
+
+
+
+
+    // State 1, Sprinting
+    private bool _isSprinting = false;
+    public void OnSprint(InputValue inputValue) {
+        _isSprinting = !_isSprinting;
+        if (_isSprinting) {
+            _speed += 5;
+        }
+        else {
+            _speed -= 5;
+        }
     }
 }
